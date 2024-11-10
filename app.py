b3f58e31ddc9a6ce077fa39849dd528e56c4decb
john-tettis <johndavidtettis@gmail.com>
2024-11-10 16:41:42 -0500

    added stop route, attempted threading integration, working on sellenium issue

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 from web import setup_browser, play_youtube_video
-
+import threading
 
 app = Flask(__name__)
 
+video_thread = None;
+global stop_video
 
 #Front end rendering
 @app.route('/home',methods=["GET"])
@@ -17,10 +19,15 @@ def home():
 def play_video():
 	driver = setup_browser()
 	link= request.args.get("link")
+	video_thread = threading.Thread(target=play_youtube_video,args=(link,driver))
 	play_youtube_video(link,driver)
 	return"link submitted"
 	
-	
+@app.route('/stop', methods=["GET"])	
+def stop_playback():
+	global stop_video
+	stop_video=True
+	return {"code":204}
 if __name__ == '__main__':
 	app.run( debug= True,host ='0.0.0.0',port=5000)
 	print("server Live")
